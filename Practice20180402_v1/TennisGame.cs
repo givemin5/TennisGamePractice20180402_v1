d0b0f86b93822581b762117dd7588335b606280e
givemin5 <givemin5@gmail.com>
2018-04-03 00:56:41 +0800

    Refactor Done

--- Practice20180402_v1/TennisGame.cs
+++ Practice20180402_v1/TennisGame.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace Practice20180402_v1
+﻿namespace Practice20180402_v1
 {
     public class TennisGame
     {
         public string Score()
         {
-            throw new NotImplementedException();
+            return "Love All";
         }
     }
 }
--- Practice20180402_v1/UnitTest1.cs
+++ Practice20180402_v1/UnitTest1.cs
@@ -5,12 +5,18 @@ namespace Practice20180402_v1
     [TestClass]
     public class TennisGameTests
     {
+        private TennisGame tennisGame = new TennisGame();
+
         [TestMethod]
         public void Love_All()
         {
-            TennisGame tennisGame = new TennisGame();
+            ScoreShouldBe("Love All");
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
             string score = tennisGame.Score();
-            Assert.AreEqual("Love All", score);
+            Assert.AreEqual(expected, score);
         }
     }
 }
