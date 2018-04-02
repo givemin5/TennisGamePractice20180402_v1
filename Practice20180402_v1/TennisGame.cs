b2e367192bb1b82fa21de7ec6d7bc90d688f8424
givemin5 <givemin5@gmail.com>
2018-04-03 00:48:10 +0800

    First Test Fail

---
+++ Practice20180402_v1/TennisGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice20180402_v1
+{
+    public class TennisGame
+    {
+        public string Score()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Practice20180402_v1/UnitTest1.cs
+++ Practice20180402_v1/UnitTest1.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Practice20180402_v1
 {
     [TestClass]
-    public class UnitTest1
+    public class TennisGameTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Love_All()
         {
+            TennisGame tennisGame = new TennisGame();
+            string score = tennisGame.Score();
+            Assert.AreEqual("Love All", score);
         }
     }
 }
