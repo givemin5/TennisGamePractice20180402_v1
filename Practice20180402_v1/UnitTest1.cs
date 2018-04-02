1b0d70eb31365416229fc5f2feb50443a090c020
givemin5 <givemin5@gmail.com>
2018-04-02 23:56:34 +0800

    加入專案檔案。

---
+++ Practice20180402_v1/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practice20180402_v1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
